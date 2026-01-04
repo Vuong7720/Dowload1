c08a2d517e8060449c45dcbe207d1fcd7ffe035d
Vuong7720 <vuong7720@gmail.com>
2026-01-04 21:01:41 +0700

    up

--- Dowload1/obj/Debug/net8.0/Dowload1.AssemblyInfo.cs
+++ Dowload1/obj/Debug/net8.0/Dowload1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dowload1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5003a7cdc36a553a70c2b79d26bcaaca23903b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dowload1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dowload1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
